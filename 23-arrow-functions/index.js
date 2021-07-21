a0229bde91da42ffe1163c6a8b4613e044f21cd9
Michelle <brendamichelle995@gmail.com>
2021-07-21 13:39:47 -0400

    before arrow functions mini lecture

---
+++ 23-arrow-functions/index.js
@@ -0,0 +1,37 @@
+function doubleIt(num) {
+    return num * 2
+}
+
+
+function createPet(nameStr, ageNum) {
+    return {
+        name: nameStr,
+        age: ageNum
+    }
+}
+
+
+
+
+/********************** forEach **********************/
+// executes a provided function once for each element
+// in the array
+
+
+
+
+/********************** MAP **********************/
+// Transforms an array
+// Returns a new array containing the transformed values
+// (Does not mutate the original array)
+// Callback should return the transformation
+
+
+
+
+
+/********************** FILTER **********************/
+// Returns a subarray satisfying a condition
+// Does not mutate the original array
+// Callback should return a boolean
+
