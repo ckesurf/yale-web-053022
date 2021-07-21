c9b217c15a3624e1260c76af3cce13b25d910c93
Michelle <brendamichelle995@gmail.com>
2021-07-21 14:36:18 -0400

    after arrow functions

--- 23-arrow-functions/index.js
+++ 23-arrow-functions/index.js
@@ -1,14 +1,51 @@
-function doubleIt(num) {
-    return num * 2
+// function doubleIt(num) {
+//     return num * 2
+// }
+
+// const doubleIt = (num) => {
+//     return num * 2
+// }
+
+// doubleIt(10) // ERROR
+
+// single lined arrow functions have implicit return without the curly braces and without the return keyword
+const doubleIt = num => num * 2
+
+
+// when implicitely returning an object, surround it with parenthesis
+const createPetFunction = (nameStr, ageNum) => ({ name: nameStr, age: ageNum })
+
+
+
+// greeting() // ok
+
+
+function greeting() {
+    console.log('Hello')
 }
 
 
-function createPet(nameStr, ageNum) {
-    return {
-        name: nameStr,
-        age: ageNum
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -18,6 +55,13 @@ function createPet(nameStr, ageNum) {
 // in the array
 
 
+// nums.forEach(function (num) {
+//     console.log(num)
+// })
+
+// nums.forEach(num => console.log(num))
+
+// nums.forEach(console.log)
 
 
 /********************** MAP **********************/
@@ -25,6 +69,12 @@ function createPet(nameStr, ageNum) {
 // Returns a new array containing the transformed values
 // (Does not mutate the original array)
 // Callback should return the transformation
+
+// const tranformedArr = nums.map(function (num) {
+//     return num * 3
+// })
+
+// const tranformedArr = nums.map(num => num * 3)
 
 
 
@@ -34,4 +84,10 @@ function createPet(nameStr, ageNum) {
 // Returns a subarray satisfying a condition
 // Does not mutate the original array
 // Callback should return a boolean
+
+
+// const filteredBooks = books.filter(function (book) {
+//     return book.yearRead < 2016
+// })
+const filteredBooks = books.filter(book => book.yearRead < 2016)
 
