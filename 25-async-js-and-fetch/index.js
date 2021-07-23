994c07f1607bde83084123d724f91b0e56c88525
Michelle <brendamichelle995@gmail.com>
2021-07-23 12:19:59 -0400

    after ajax/fetch lecture

--- 25-async-js-and-fetch/index.js
+++ 25-async-js-and-fetch/index.js
@@ -6,14 +6,47 @@ function sleep(time) {
 }
 
 
-document.querySelector('button').addEventListener('click', () => {
-    console.log("CLICKED !!!")
+// document.querySelector('button').addEventListener('click', () => {
+//     console.log("CLICKED !!!")
+// })
+
+
+// // translated Ruby code from synch.rb
+// console.log("Hello World!")
+// console.log("brb, gonna go sleep for a sec")
+// // sleep(5)
+
+// setTimeout(() => {
+//     console.log('inside of timeout')
+// }, 5000)
+
+// console.log("What a nice cat nap that was!")
+
+
+console.log('before fetch')
+
+// fetch('https://randomfox.ca/floof/')
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+
+
+
+
+
+const btn = document.querySelector('#get-fox-btn')
+
+btn.addEventListener('click', () => {
+    
+    fetch('https://randomfox.ca/floof/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            // DOM MANIPULATION
+            const img = document.querySelector('img')
+            console.log(img)
+            img.src = data.image
+        })
+
+
 })
-
-
-// translated Ruby code from synch.rb
-console.log("Hello World!")
-console.log("brb, gonna go sleep for a sec")
-// sleep(5)
-
-console.log("What a nice cat nap that was!")
