345dea5c5eb6c4ec204fb2a409503415fc7894ac
Michelle <brendamichelle995@gmail.com>
2021-07-27 09:45:42 -0400

    before object shorthand lecture

---
+++ 27-object-shorthand/index.js
@@ -0,0 +1,26 @@
+// Lecture: Object Shorthand Notation
+
+const firstName = 'Raffy'
+const age = 9
+const isTroubleMaker = true
+const weaknesses = ['biscuits', 'birds']
+const strengths = ['cuteness', 'walking']
+
+
+const raffy = {
+    firstName: firstName,
+    age: age,
+    isTroubleMaker: isTroubleMaker,
+    weaknesses: weaknesses,
+    strengths: strengths
+}
+
+
+
+
+
+
+
+
+
+
