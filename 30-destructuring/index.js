8dfc58e6db7f8cdd9682c363b7c14d9c79297dbf
Michelle <brendamichelle995@gmail.com>
2021-08-03 16:44:34 -0400

    after destructuring mini

--- 30-destructuring/index.js
+++ 30-destructuring/index.js
@@ -16,10 +16,17 @@ const raffy = {
 }
 
 // original way
+// const name = raffy.name
+// const age = raffy.age
+// const bestFriends = raffy.bestFriends
+
+
+// destructuring syntax
+const { name, age, type, bestFriends } = raffy
 
 
 
-
+const { fruits: { orange: { color } } } = raffy
 
 
 
@@ -44,6 +51,20 @@ const apples = ["green", "yellow", "purple", ["rainbow", "blue"]]
 
 // original way
 
+// const b1 = cryptoCurrencies[0]
+// const dash = cryptoCurrencies[1]
+// const eth = cryptoCurrencies[2]
+
+// const nestedArr = apples[3]
+// const rnb = apples[3][0]
+
+
+// destructuring syntax
+
+const [bc, , , , mno] = cryptoCurrencies
+
+// const [g, y, p, nestedArr] = apples
+const [g, y, p, [rb, bl]] = apples
 
 
 
@@ -79,15 +100,19 @@ const pTags = document.querySelectorAll('p')
 
 // original way
 
+// const firstLi = liTags[0]
+// const secondLi = liTags[1]
+// const thirdLi = liTags[2]
 
 
 
+// destructuring
+
+const [first, second, third] = liTags
 
 
 
-
-
-
+const [firstP, secondP, thirdP] = pTags
 
 
 
@@ -121,7 +146,14 @@ function getMovieTitles() {
 }
 
 // original way
-const arr = getMovieTitles()
+// const arr = getMovieTitles()
+// const ropa = arr[0]
+// const dopa = arr[1]
+// const wfpa = arr[2]
+
+
+// destructuring syntax
+const [ropa, dopa, wfpa] = getMovieTitles()
 
 
 
@@ -158,13 +190,25 @@ const arr = getMovieTitles()
 
 
 /********************** Function Example #2 **********************/
-function shareContactInfo(empObj) {
-    const name = empObj.name
-    const position = empObj.position
-    const cell = empObj.phones.cell
-    const work = empObj.phones.work
+// function shareContactInfo(empObj) {
+//     // const name = empObj.name
+//     // const position = empObj.position
+//     // const cell = empObj.phones.cell
+//     // const work = empObj.phones.work
 
-    return `${name} is an ${position} who can be reached by cell at 
+//     return `${empObj.name} is an ${empObj.position} who can be reached by cell at 
+//     ${empObj.phones.cell} or by landline at ${empObj.phones.work}`
+// }
+
+function shareContactInfo({ name: otherNameVariable, position = "Software Engineer", phones: { cell, work } }) {
+    // const name = empObj.name
+    // const position = empObj.position
+    // const cell = empObj.phones.cell
+    // const work = empObj.phones.work
+    // console.log(name, position, phones)
+    console.log(otherNameVariable)
+
+    return `${otherNameVariable} is an ${position} who can be reached by cell at 
     ${cell} or by landline at ${work}`
 }
 
@@ -179,7 +223,7 @@ const e1 = {
 
 const e2 = {
     name: "Greg",
-    position: "Instructor",
+    job: "Instructor",
     phones: {
         cell: "555-987-3455",
         work: "555-345-6677"
