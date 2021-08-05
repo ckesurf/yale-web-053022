8281200f07a1439751531ec06522f926c7c3e316
Michelle <brendamichelle995@gmail.com>
2021-08-05 10:55:33 -0400

    after async await mini

--- 33-aync-await/index.js
+++ 33-aync-await/index.js
@@ -1,7 +1,34 @@
+// async await 
+
 function getFoxPic() {
-    fetch("https://randomfox.ca/floof/")
-        .then(response => response.json())
-        .then(data => console.log(data))
+  fetch("https://randomfox.ca/floof/")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  console.log(data)
 }
 
-  // getFoxPic()
+// async function getFoxPic() {
+//   const response = await fetch("https://randomfox.ca/floof/")
+//   const data = await response.json()
+//   console.log(data)
+// }
+function foo(data) {
+  console.log(data)
+}
+
+
+
+const getFoxPic = async () => {
+  const response = await fetch("https://randomfox.ca/floof/")
+  const data = await response.json()
+  return data
+}
+
+
+// const data = await getFoxPic() // WILL NOT WORK - await can only be used
+// inside of asyn functions
+
+getFoxPic().then(data => console.log(data))
+
+console.log("hello")
